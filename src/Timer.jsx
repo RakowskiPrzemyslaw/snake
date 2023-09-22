6145cc992ed1b1e3ab91910cf8cacaf2faf52920
Kamil Zajac <k.zajac@livechatinc.com>
2023-09-22 10:40:48 +0200

    wordprogress-board

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -3,7 +3,9 @@ import { roundAtom, roundTimeAtom } from "./context/game";
 import { Text } from "@react-three/drei";
 import game from '../game.json';
 
-const COLOR = '#000000';
+const COLOR = '#ffffff';
+const WIDTH = game.size.x * 2 + 1;
+const HEIGHT = 1.2;
 
 export function Timer() {
   const [roundTime] = useAtom(roundTimeAtom);
@@ -15,18 +17,18 @@ export function Timer() {
 
   return (
     <group>
-      {/* <mesh position={[0, 0, -0.5]} receiveShadow castShadow>
-        <planeGeometry args={[width, height, width, height]} />
+      <mesh position={[0, -11.5, -0.5]} receiveShadow castShadow>
+        <planeGeometry args={[WIDTH, HEIGHT, WIDTH, HEIGHT]} />
         <meshStandardMaterial color="#5c5c5c" />
-      </mesh> */}
+      </mesh>
 
-      <mesh position={[-3, -11.5, 0]}>
+      <mesh position={[-3, -11.6, 0]}>
         <circleGeometry
-          args={[0.4, 32, Math.PI / 2, 2 * Math.PI * timeLeftPercentage]}
+          args={[0.35, 32, Math.PI / 2, 2 * Math.PI * timeLeftPercentage]}
         />
         <meshBasicMaterial color={COLOR} fillOpacity={100} />
         { word.map((char, index) => (
-          <Text font="/noto.json" key={index} fillOpacity={char.isCollected ? 100 : 0.5} color={COLOR} fontSize={1} position={[1.2 + index, 0, 1]}>{char.code}</Text>))}
+          <Text font="/noto.json" key={index} fillOpacity={char.isCollected ? 100 : 0.5} color={COLOR} fontSize={0.7} position={[1 + index, 0, 1]}>{char.code}</Text>))}
       </mesh>
     </group>
   );
